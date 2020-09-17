9d5eee19dc6e85df3c978579912dbfa743b8bf4e
roberta-fiume <robertaf898@gmail.com>
2020-09-17 11:47:03 +0100

    added structure

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,117 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>WELCOME TO QUIZPAW!</h1>
+
+    <h2>Best Bear quiz results:</h2>
+
+    <div class="bears">
+      <div class="grizzly">
+        <h1>Grizzly</h1>
+        <h2>TOTAL: </h2>
+        <p>QuizPaw: </p>
+        <p>Frienderly: </p>
+        <p>ConnectMe: </p>
+      </div>
+
+      <div class="panda">
+        <h1>Panda</h1>
+        <h2>TOTAL: </h2>
+        <p>QuizPaw: </p>
+        <p>Frienderly: </p>
+        <p>ConnectMe: </p>
+      </div>
+
+      <div class="polar">
+        <h1>Polar</h1>
+        <h2>TOTAL: </h2>
+        <p>QuizPaw: </p>
+        <p>Frienderly: </p>
+        <p>ConnectMe: </p>
+      </div>
+
+      <div class="BlackBear">
+        <h1>Black Bear</h1>
+        <h2>TOTAL: </h2>
+        <p>QuizPaw: </p>
+        <p>Frienderly: </p>
+        <p>ConnectMe: </p>
+      </div>
+    
+    </div>
+    
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      frienderly: [
+        {
+          option:"Grizzly",
+          votes: 201
+        },
+        {
+          option:"Panda",
+          votes: 195
+        },
+        {
+          option:"Polar",
+          votes: 304
+        },
+        {
+          option: "Black Bear",
+          votes: 236
+        }
+      ],
+
+      connectMe: [
+        {
+          option:"Grizzly",
+          votes: 145
+        },
+        {
+          option:"Panda",
+          votes: 349
+        },
+        {
+          option:"Polar",
+          votes: 431
+        },
+        {
+          option: "Black Bear",
+          votes: 219
+        }
+      ],
+
+      quizPaw: {
+        grizzly: 218,
+        panda: 353,
+        polar: 237,
+        blackBear: 161
+      }
+    }
+  },
+  methods: {
+
+  },
+
+  computed: {
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.bears {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
 </style>
